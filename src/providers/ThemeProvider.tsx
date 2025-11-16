4d66d4f0ed5229dccc4616f0ee058f9d5c3f649c
kong <piorag99@gmail.com>
2025-11-17 02:53:07 +0900

    chore: README 업데이트

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,9 +1,4 @@
-import React, {
-	createContext,
-	type ReactNode,
-	useContext,
-	useState,
-} from "react";
+import { createContext, type ReactNode, useContext, useState } from "react";
 
 interface ThemeContextType {
 	isDark: boolean;
