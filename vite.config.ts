c0f61fa57bd8ab0cac373fd43be43882328fd18a
kong <piorag99@gmail.com>
2025-11-16 20:44:19 +0900

    init : 절대 경로 설정, 바닐라 익스트랙트 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), vanillaExtractPlugin()],
+	resolve: {
+		alias: [
+			{ find: "@apis", replacement: "/src/apis" },
+			{ find: "@assets", replacement: "/src/assets" },
+			{ find: "@pages", replacement: "/src/pages" },
+			{ find: "@providers", replacement: "/src/providers" },
+			{ find: "@routes", replacement: "/src/routes" },
+			{ find: "@shared", replacement: "/src/shared" },
+			{ find: "@styles", replacement: "/src/styles" },
+		],
+	},
+});
