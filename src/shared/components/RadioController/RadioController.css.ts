8a76a93e8e208bce4195e01d0dc5fed6c8b7df78
kong <piorag99@gmail.com>
2025-11-22 05:27:09 +0900

    feat: RadioController 제작

---
+++ src/shared/components/RadioController/RadioController.css.ts
@@ -0,0 +1,16 @@
+import { style } from "@vanilla-extract/css";
+
+export const radioControllerContainer = style({
+	width: "100%",
+	padding: "0",
+	marginTop: "2rem",
+});
+
+export const radioController = style({
+	width: "100%",
+	marginTop: "2rem",
+});
+
+export const radioControllerLabel = style({
+	marginBottom: "1rem",
+});
---
+++ src/shared/components/RadioController/RadioController.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Radio, RadioGroup, Text, View } from "reshaped";
+import * as styles from "./RadioController.css";
+
+interface RadioItem {
+	label: string;
+	value: string;
+}
+
+interface RadioControllerProps {
+	name: string;
+	radioItems: RadioItem[];
+	valueRef: React.RefObject<string>;
+}
+
+function RadioController(props: RadioControllerProps) {
+	const { name, radioItems, valueRef } = props;
+	const onChange = ({ value }: { value: string }) => {
+		valueRef.current = value;
+	};
+	return (
+		<Container className={styles.radioControllerContainer}>
+			<Text variant={"featured-1"}>{name}</Text>
+			<View direction={"row"} gap={2} className={styles.radioController}>
+				<RadioGroup name={name} onChange={onChange}>
+					{radioItems.map((item, idx) => (
+						<Radio key={`${item.value}-${idx}`} size="large" value={item.value}>
+							{item.label}
+						</Radio>
+					))}
+				</RadioGroup>
+			</View>
+		</Container>
+	);
+}
+
+export default RadioController;
